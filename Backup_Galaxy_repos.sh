7ae02efec0894b5ea2827327c92a569931daab80
dickgroenenberg <dickgroenenberg@users.noreply.github.com>
2020-03-23 13:59:33 +0100

    Add files via upload

---
+++ Backup_Galaxy_repos.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# backup Naturalis Github repos for all Tools on Galaxy:
+
+git clone https://github.com/naturalis/galaxy-tool-flash.git									# Merge reads
+git clone https://github.com/naturalis/galaxy-tool-cutadapt.git									# Trim primers
+git clone https://github.com/naturalis/galaxy-tool-make-otu-table.git							# Make otu table
+git clone https://github.com/naturalis/galaxy-tool-otutab_trim.git								# Trim otu table
+git clone https://github.com/naturalis/galaxy-tool-nucleotide-translation.git					# Translate nucleotide sequences
+git clone https://github.com/naturalis/galaxy-tool-otu-table-freq-filter.git					# Otu table frequency filter
+git clone https://github.com/naturalis/galaxy-tool-add-metadata-otutable.git					# Add metadata to otu table
+git clone https://github.com/naturalis/galaxy-tool-manage-zip.git								# Manage ZIP
+git clone https://github.com/naturalis/galaxy-tool-split-on-primer.git							# Split on primer
+git clone https://github.com/naturalis/galaxy-tool-prinseq-sequence-trimmer.git					# Sequence trimmer
+git clone https://github.com/naturalis/galaxy-tool-lca.git										# lowest common ancestor
+git clone https://github.com/naturalis/galaxy-tool-UMI-isolation.git							# Cluster on UMIs:
+git clone https://github.com/naturalis/galaxy-tool-taxonomic-accumulation.git					# Taxonomic accumulator
+git clone https://github.com/naturalis/galaxy-tool-BLAST.git									# Identify reads with blastn and find taxonomy
+git clone https://github.com/naturalis/galaxy-tool-BLAST.wiki.git
+git clone https://github.com/naturalis/galaxy-tool-sickle-pe.git								# Quality trim paired-end reads with Sickle
+git clone https://github.com/naturalis/galaxy-tool-taxonmatcher.git								# taxonmatcher
+git clone https://github.com/naturalis/galaxy-tool-prinseq-sequence-analysis.git				# Sequence analyzer
+git clone https://github.com/naturalis/galaxy-tool-spreading-correction.git						# Correct spreading V2
+git clone https://github.com/naturalis/Galaxy-Installation.git									# Install Galaxy
+git clone https://github.com/naturalis/openstack-workshop.git									# OpenStack workshop 2018 Atze
+
+# Total number of repo's:	19
+
